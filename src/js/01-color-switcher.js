4a5daf8454cd8ec3aea22889d4a9a88f01125d27
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-09-06 14:29:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyEl: document.body,
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStoptBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute("disabled", true);
+  
+  timerId = setInterval(() => {
+    console.log('Change backgground color');
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000); 
+};
+
+function onStoptBtnClick() {
+  refs.startBtn.removeAttribute("disabled"); 
+
+  clearInterval(timerId);
+  console.log('Stop change');
+};
