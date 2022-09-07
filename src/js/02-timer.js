0afab5c75b2d57ae5ec386e3625e4d33d902ffbe
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-09-07 22:24:43 +0300

    добавила task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,107 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.setAttribute("disabled", true);
+
+const currentDate = Date.now();
+const fp = flatpickr("#datetime-picker", options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(`Выбранная дата: ${selectedDates[0]}`);
+        
+        if (selectedDates[0] > currentDate) {
+            refs.startBtn.removeAttribute("disabled");
+        } else {
+            refs.startBtn.setAttribute("disabled", true);
+            window.alert("Please choose a date in the future");
+        }
+    }
+});
+
+class Timer {
+    constructor({onTick}) {
+        this.intervalId = null;
+        this.isActive = false;
+        this.onTick = onTick;
+    }
+
+    start() {
+        if (this.isActive) {
+            return;
+        }
+
+        const startTime = Date.now();
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const ms = currentTime - startTime;
+            const time = this.convertMs(ms);
+            
+            this.onTick(time);
+            //console.log(`${days}:${hours}:${minutes}:${seconds}`);
+        }, 1000);
+    }
+
+    //Принимает время в милисекундах,
+    //высчитывает сколько в них вмещается дней/часов/минут/секунд,
+    //возвращает объект со свойствами days, hours, minutes, seconds
+    convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+    };
+
+    //Принимает число, приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
+    addLeadingZero(value) {
+        return String(value).padStart(2, '0');
+    };
+}
+
+const timer = new Timer({
+    onTick: updateClockface
+});
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+
+//Принимает декструктизированное время, рисует интерфейс
+function updateClockface({ days, hours, minutes, seconds }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.minutes.textContent = `${minutes}`;
+    refs.seconds.textContent = `${seconds}`;
+}
+
+
+
+
+
+
+
+
